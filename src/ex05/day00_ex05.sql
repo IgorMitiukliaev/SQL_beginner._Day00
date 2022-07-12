a14068dc8cb2629d8e083b492646a76fbc80996f
wloyd <wloyd@student.21-school.ru>
2022-07-12 16:08:12 +0700

    ~

--- src/ex05/day00_ex05.sql
+++ src/ex05/day00_ex05.sql
@@ -1,4 +1,4 @@
-SELECT DISTINCT person.name
+SELECT DISTINCT person.name, sub.order_date, sub.menu_id
 FROM person,
     (
         SELECT DISTINCT person_id,
@@ -10,6 +10,20 @@ FROM person,
                 OR menu_id = 14
                 OR menu_id = 18
             )
-            OR (order_date = '2022-01-07')
+            AND (order_date = '2022-01-07')
     ) AS sub
 WHERE person.id = sub.person_id;
+
+
+SELECT (
+        SELECT name
+        FROM person
+        WHERE person.id = person_order.person_id
+    ) AS NAME, order_date, person_id, menu_id
+FROM person_order
+WHERE (
+        person_order.menu_id = 13
+        OR person_order.menu_id = 14
+        OR person_order.menu_id = 18
+    )
+    AND person_order.order_date = '2022-01-07';
